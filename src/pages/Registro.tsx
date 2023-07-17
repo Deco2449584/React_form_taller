60c2ad1c1cf4319949a38b8339b2f11f89c5d11e
Daniel Caro <deco2449584@gmail.com>
2023-07-17 15:39:06 -0500

    se hizo build

--- src/pages/Registro.tsx
+++ src/pages/Registro.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import PersonalData from "../components/Registro/PersonalData";
 import AcademicData from "../components/Registro/AcademicData";
@@ -9,22 +9,8 @@ import AccountData from "../components/Registro/AccountData";
 import Summary from "../components/Registro/Sumary";
 
 const Registro: React.FC = () => {
-  const navigate = useNavigate();
-
-  const handleComenzarRegistro = () => {
-    navigate("/Registro/PersonalData");
-  };
-
   return (
     <div className="registration-page">
-      {/* <h1 className="text-2xl font-bold mb-4">Registro</h1>
-      <p className="text-gray-600 mb-4">Bienvenido al proceso de registro.</p>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleComenzarRegistro}
-      >
-        Comenzar el registro
-      </button> */}
       <Routes>
         <Route path="/PersonalData" element={<PersonalData />} />
         <Route path="/AcademicData" element={<AcademicData />} />
